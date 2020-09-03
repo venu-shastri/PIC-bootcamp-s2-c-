b4af5b4e0442e2628adff0ef252accc1fee165eb
Venugopal K <venu.shastri@gmail.com>
2020-09-03 17:48:08 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Namespace
+namespace Demo
+{
+    #region Used Defind Types
+    /*Default Visibility - internal */
+    class Calculator
+    {
+        #region Methods
+         private  int  Add(int x,int y)
+        {
+            return x + y;
+        }
+        #endregion
+    }
+    #endregion
+}
+#endregion
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    //Full Name - Demo.Program
+    class Program /*Reference Type */
+    {
+
+        static void Main()
+        {
+            System.Console.WriteLine("C# Sample Demo App");
+            Calculator obj = new Calculator();
+            var method=obj.GetType().GetMethod("Add",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            object result=method.Invoke(obj, new Object[] { 10, 20 });
+            Console.WriteLine(result);
+        }
+    }
+}
