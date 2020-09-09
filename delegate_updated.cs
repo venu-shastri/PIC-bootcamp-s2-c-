2d2a54f0502b40c3b7809b45437a23f751ed8488
Venugopal K <venu.shastri@gmail.com>
2020-09-09 09:46:40 +0530

    Create delegate_updated.cs

---
+++ delegate_updated.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhyFunctionPointers
+{
+  
+    public class EmailTask
+    {
+        public  void Send()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Sending Email.....");
+            }
+        }
+    }
+
+    public class SMSTask
+    {
+        public  void Send()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Sending SMS.....");
+            }
+        }
+    }
+    public class WhatsupTask
+    {
+
+    }
+
+    public delegate void ThreadStartCommand();
+    public delegate void ParameterizedThreadStartCommand(object argument);
+
+    /*
+    public class ThreadStartCommand
+    {
+        object target;
+        string methodName;
+
+        public ThreadStartCommand(object commandTarget,string methodName)
+        {
+            this.target = commandTarget;
+            this.methodName = methodName;
+        }
+        public void Invoke()
+        {
+            //Reflection - Dynamically  Discover - Object / DataType Details
+            // method existance - Given Object
+
+         System.Type _typeDef =  this.target.GetType();
+          System.Reflection.MethodInfo methodInfo=  _typeDef.GetMethod(this.methodName);
+            //Invoke - Dynamic Invoke
+            methodInfo.Invoke(this.target,null);
+
+
+        }
+    }
+    */
+    public class Thread {
+
+        ThreadStartCommand _startCommand;
+        ParameterizedThreadStartCommand _parameterizedStartCommand;
+        public Thread(ThreadStartCommand startCommand)
+        {
+            this._startCommand = startCommand;
+        }
+        public Thread(ParameterizedThreadStartCommand startCommand)
+        {
+            this._parameterizedStartCommand = startCommand;
+        }
+        public void Start() {
+
+            this._startCommand.Invoke();
+        }
+        public void Start(object parameter)
+        {
+            this._parameterizedStartCommand.Invoke(parameter);
+        }
+        public void Stop() { }
+    }
+
+    
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailTask _emailTask = new EmailTask();
+            ThreadStartCommand _emailTaskCommand = new ThreadStartCommand(_emailTask.Send);
+            Thread _t1 = new Thread(_emailTaskCommand);
+            _t1.Start();
+
+            SMSTask _smsTask = new SMSTask();
+            ThreadStartCommand _smsTaskCommand = new ThreadStartCommand(_smsTask.Send);
+            Thread _t2 = new Thread(_smsTaskCommand);
+            _t2.Start();
+        }
+    }
+}
